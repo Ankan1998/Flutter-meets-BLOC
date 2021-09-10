f0bc5d26a429fdcc50b61b987ea115c52dac2c9c
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-10 17:55:35 +0530

    model testing to json uploaded

--- blocflutter/test/movie_model_test.dart
+++ blocflutter/test/movie_model_test.dart
@@ -7,11 +7,22 @@ import 'movie_data.dart';
 
 void main(){
 
-  test('Model testing', () {
+  test('Model testing from json', () {
     Moviemodel moviemodel = Moviemodel.fromJson(
         json.decode(MovieMockData.GET_MOVIE_RESPONSE)
     );
     expect(moviemodel.id, 'tt1375666');
     expect(moviemodel.trailer.id, 'vi4219471385');
+  });
+
+  test('Model testing to json', () {
+    Moviemodel moviemodel = Moviemodel.fromJson(
+        json.decode(MovieMockData.GET_MOVIE_RESPONSE)
+    );
+    moviemodel.toJson();
+    moviemodel.trailer.toJson();
+    moviemodel.cast.forEach((element) {
+      element.toJson();
+    });
   });
 }
