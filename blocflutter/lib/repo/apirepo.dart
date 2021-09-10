bcc12af6b1acca619a6bb4a89f1e0c67a40d26b2
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-10 14:09:58 +0530

    Fetching movie data from API

--- blocflutter/lib/repo/apirepo.dart
+++ blocflutter/lib/repo/apirepo.dart
@@ -1,5 +1,18 @@
 import 'package:http/http.dart' as http;
 
 class Apirepo {
+  Future<dynamic> getMovieData() async {
+    Map<String, String> requestHeaders = {
+      'x-rapidapi-host':
+          'imdb-internet-movie-database-unofficial.p.rapidapi.com',
+      'x-rapidapi-key': 'f525b2fab4mshdbb0c7c17eac930p1273efjsna765b0e6eb9e',
+    };
 
+    http.Response response = await http.get(
+        Uri.parse(
+            'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666'),
+        headers: requestHeaders);
+
+    return response.body;
+  }
 }
