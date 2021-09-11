fdb7e4b2ce0a1eae94e15799311f9baa7ccf786f
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-11 13:02:10 +0530

    search movie mock data added

---
+++ blocflutter/test/search_movie_data.dart
@@ -0,0 +1,44 @@
+class SearchMovieMockData{
+  SearchMovieMockData._();
+
+  static const String GET_SEARCH_MOVIE_RESPONSE = """
+  {
+      "titles": [
+          {
+              "title": "Joker",
+              "image": "https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@.jpg",
+              "id": "tt7286456"
+          },
+          {
+              "title": "Wild Card",
+              "image": "https://m.media-amazon.com/images/M/MV5BMjAyNjkyOTgzNF5BMl5BanBnXkFtZTgwMTk0MDc2MzE@.jpg",
+              "id": "tt2231253"
+          }
+      ],
+      "names": [
+          {
+              "title": "Jesse Flanagan",
+              "image": "https://m.media-amazon.com/images/S/sash/N1QWYSqAfSJV62Y.png",
+              "id": "nm0281041"
+          },
+          {
+              "title": "Jake Bern",
+              "image": "https://m.media-amazon.com/images/M/MV5BNjExZGRlY2YtYjQ5NS00ZGRkLWJiYWUtOTBjNGIwNjY4ZWJiXkEyXkFqcGdeQXVyMjQwMDg0Ng@@.jpg",
+              "id": "nm1502206"
+          }
+      ],
+      "companies": [
+          {
+              "title": "Joker",
+              "image": "https://m.media-amazon.com/images/M/MV5BNjExZGRlY2YtYjQ5NS00ZGRkLWJiYWUtOTBjNGIwNjY4ZWJiXkEyXkFqcGdeQXVyMjQwMDg0Ng@@.jpg",
+              "id": "co0014094"
+          },
+          {
+              "title": "Joker",
+              "image": "https://m.media-amazon.com/images/M/MV5BNjExZGRlY2YtYjQ5NS00ZGRkLWJiYWUtOTBjNGIwNjY4ZWJiXkEyXkFqcGdeQXVyMjQwMDg0Ng@@.jpg",
+              "id": "co0208239"
+          }
+      ]
+  }
+  """;
+}
