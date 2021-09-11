6a2676c0265ad27e1709dc21f3c17e67b15941f6
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-11 13:14:51 +0530

    search movie model test added

---
+++ blocflutter/test/search_movie_model_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:blocflutter/model/search_movie_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'search_movie_data.dart';
+
+void main(){
+
+  test('Search movie Model testing from json', () {
+    Searchmovie searchmoviemodel = Searchmovie.fromJson(
+        json.decode(SearchMovieMockData.GET_SEARCH_MOVIE_RESPONSE)
+    );
+    expect(searchmoviemodel.titles[0].id, 'tt7286456');
+    expect(searchmoviemodel.names[0].id, 'nm0281041');
+    expect(searchmoviemodel.companies[0].id, 'co0014094');
+  });
+
+  test('Search Movie Model testing to json', () {
+    Searchmovie searchmoviemodel = Searchmovie.fromJson(
+        json.decode(SearchMovieMockData.GET_SEARCH_MOVIE_RESPONSE)
+    );
+    searchmoviemodel.toJson();
+    searchmoviemodel.titles.forEach((element) {
+      element.toJson();
+    });
+    searchmoviemodel.names.forEach((element) {
+      element.toJson();
+    });
+    searchmoviemodel.companies.forEach((element) {
+      element.toJson();
+    });
+  });
+}
