12a5b75e306e7545abaf0f7f282349d14679d51c
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-11 12:46:20 +0530

    Api repo added with search movie option

--- blocflutter/lib/repo/apirepo.dart
+++ blocflutter/lib/repo/apirepo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class Apirepo {
@@ -11,6 +13,24 @@ class Apirepo {
     http.Response response = await http.get(
         Uri.parse(
             'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666'),
+        headers: requestHeaders);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print("Error in Fetching Data");
+    }
+  }
+
+  Future<dynamic> searchMovie() async {
+    Map<String, String> requestHeaders = {
+      'x-rapidapi-host':
+      'imdb-internet-movie-database-unofficial.p.rapidapi.com',
+      'x-rapidapi-key': 'f525b2fab4mshdbb0c7c17eac930p1273efjsna765b0e6eb9e',
+    };
+
+    http.Response response = await http.get(
+        Uri.parse(
+            'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/joker'),
         headers: requestHeaders);
     if (response.statusCode == 200) {
       return response.body;
