3ee51c51e95538c868a06f5a58c1ce40d4e14df0
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-12 21:24:29 +0530

    movie_slider issue with fitted box resolved

--- blocflutter/lib/widgets/movie_slider.dart
+++ blocflutter/lib/widgets/movie_slider.dart
@@ -15,20 +15,26 @@ class _MovieSliderState extends State<MovieSlider> {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider.builder(
+
       options: CarouselOptions(
+        initialPage: 0,
         enlargeCenterPage: true,
-        aspectRatio: 16/9,
         autoPlay: true,
         height: MediaQuery.of(context).size.height * 0.7
       ),
       itemCount: widget.searchmovie.titles.length,
       itemBuilder: (context, index,pindex) {
-        return FittedBox(
-            fit: BoxFit.fill,
-          child: Image.network(
-            widget.searchmovie.titles[index].image,
+        // print(widget.searchmovie.titles[index].title);
+        // return FittedBox(
+        //   fit: BoxFit.fill,
+        //   child: Image.network(
+        //     widget.searchmovie.titles[index].image,
+        //
+        //   )
+        return Image.network(
+          widget.searchmovie.titles[index].image,
+          fit: BoxFit.fill,
 
-          )
         );
 
       },
