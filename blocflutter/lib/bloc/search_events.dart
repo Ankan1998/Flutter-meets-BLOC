44b26aa35fcfd4ce71f26b3220033265290b9902
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-16 22:05:09 +0530

    search state and event updated with arguments

--- blocflutter/lib/bloc/search_events.dart
+++ blocflutter/lib/bloc/search_events.dart
@@ -4,13 +4,13 @@ class SearchEvent extends Equatable {
   @override
   // TODO: implement props
   List<Object> get props => [];
- 
 }
 
 class SearchMovie extends SearchEvent {
+  final _search;
 
+  SearchMovie(this._search);
+  List<Object> get props => [_search];
 }
 
-class ResetSearch extends SearchEvent{
-  
-}
+class ResetSearch extends SearchEvent {}
--- blocflutter/lib/bloc/search_state.dart
+++ blocflutter/lib/bloc/search_state.dart
@@ -1,24 +1,21 @@
 import 'package:equatable/equatable.dart';
 
-abstract class SearchState extends Equatable{
+abstract class SearchState extends Equatable {
   @override
   // TODO: implement props
   List<Object> get props => [];
+}
+
+class SearchInitState extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchLoaded extends SearchState {
+  final _searchloaded;
+
+  SearchLoaded(this._searchloaded);
+  List<Object> get props => [_searchloaded];
 
 }
 
-class SearchInitState extends SearchState {
-
-}
-
-class SearchLoading extends SearchState {
-
-}
-
-class SearchLoaded extends SearchState{
-
-}
-
-class SearchError extends SearchState{
-  
-}
+class SearchError extends SearchState {}
