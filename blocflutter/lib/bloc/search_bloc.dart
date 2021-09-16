ecb68a2280e951413054fca08a8a85361361ba94
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-16 22:06:17 +0530

    search bloc added

---
+++ blocflutter/lib/bloc/search_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:blocflutter/bloc/search_events.dart';
+import 'package:blocflutter/bloc/search_state.dart';
+import 'package:blocflutter/model/search_movie_model.dart';
+import 'package:blocflutter/repo/search_movie_api.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  final SearchApiRepo searchrepo;
+
+  SearchBloc(this.searchrepo) : super(SearchInitState());
+
+  @override
+  Stream<SearchState> mapEventToState(SearchEvent event) async* {
+    if (event is SearchMovie) {
+      yield SearchLoading();
+      try {
+        var search_ = await searchrepo.searchMovie();
+        SearchModel search_model = SearchModel.fromJson(search_);
+        yield SearchLoaded(search_model);
+      } catch (e) {
+        yield SearchError();
+      }
+    } else if (event is ResetSearch) {
+      yield SearchInitState();
+    }
+  }
+}
