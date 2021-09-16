995f37d0324fb53bf3b250a524ff519ef8927bcb
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-16 20:50:11 +0530

    Search Event added

---
+++ blocflutter/lib/bloc/search_events.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class SearchEvent extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+ 
+}
+
+class SearchMovie extends SearchEvent {
+
+}
+
+class ResetSearch extends SearchEvent{
+  
+}
