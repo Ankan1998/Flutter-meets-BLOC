e579008fe25f5cc384536c1ad5ee67c46a16d7f5
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-18 18:47:03 +0530

    bypassing splashscreen for testing

--- blocflutter/lib/main.dart
+++ blocflutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:blocflutter/bloc/search_bloc.dart';
 import 'package:blocflutter/repo/search_movie_api.dart';
+import 'package:blocflutter/screen/home_screen.dart';
 import 'package:blocflutter/screen/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       home: BlocProvider(
         create: (context) => SearchBloc(SearchApiRepo()),
-        child: SplashScreenx(),
+        child: HomeScreen(),
       ),
     );
   }
