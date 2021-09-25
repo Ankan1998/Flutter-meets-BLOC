52c709b256f0215c1f0ef4207266bb078cf4f552
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-25 13:08:38 +0530

    movie details updated

--- blocflutter/lib/screen/movie_details.dart
+++ blocflutter/lib/screen/movie_details.dart
@@ -1,4 +1,5 @@
 import 'package:blocflutter/model/movie_model.dart';
+import 'package:blocflutter/themes.dart';
 import 'package:blocflutter/widgets/stack_movie_detail.dart';
 import 'package:flutter/material.dart';
 
@@ -11,34 +12,56 @@ class MovieDetails extends StatefulWidget {
 }
 
 class _MovieDetailsState extends State<MovieDetails> {
+
+  Widget movie_poster(){
+    return Align(
+      alignment: Alignment.center,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.5,
+        width: MediaQuery.of(context).size.width * 1.0,
+        child: Image.network(
+          widget.movie.poster,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Widget releaseyear() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.6,
+      height: MediaQuery.of(context).size.height * 0.1,
+      decoration: BoxDecoration(
+        color: Color(0x332B9BAB),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        border: Border.all(color: Colors.white, width: 3),
+      ),
+      child: Center(
+        child: Text(
+          "Release Year: ${widget.movie.year}",
+          style: MyTheme.heading,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // appBar: AppBar(
-        //   title: Text(widget.movie.title),
-        //   centerTitle: true,
-        // ),
+        backgroundColor: Theme.of(context).primaryColor,
         body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  width: MediaQuery.of(context).size.width * 1.0,
-                  
-                  child: Image.network(
-                    widget.movie.poster,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              StackMovie(moviemodel: widget.movie,),
-            ],
-          )
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            movie_poster(),
+            StackMovie(
+              moviemodel: widget.movie,
+            ),
+            SizedBox(height: 5),
+            releaseyear(),
+          ],
+        )),
       ),
     );
   }
