094cacb872564099dc611eba71da940f95a28004
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-26 21:51:38 +0530

    bloc provider for movie bloc added [will further update]

--- blocflutter/lib/widgets/movie_grid.dart
+++ blocflutter/lib/widgets/movie_grid.dart
@@ -1,11 +1,12 @@
+import 'package:blocflutter/bloc/moviebloc/movie_bloc.dart';
+import 'package:blocflutter/bloc/moviebloc/movie_event.dart';
 import 'package:blocflutter/model/movie_model.dart';
 import 'package:blocflutter/model/search_movie_model.dart';
 import 'package:blocflutter/repo/movie_details_api.dart';
 import 'package:blocflutter/screen/movie_details.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MovieGrid extends StatefulWidget {
   const MovieGrid({Key key, this.searchmovie}) : super(key: key);
@@ -16,43 +17,48 @@ class MovieGrid extends StatefulWidget {
 }
 
 class _MovieGridState extends State<MovieGrid> {
-  Moviemodel moviemodel;
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,       
-      ),
-      itemCount: widget.searchmovie.titles.length,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: GestureDetector(
-            onTap: () async {
-              final mr = new MovieApiRepo(
-                  movie_id: widget.searchmovie.titles[index].id);
-              var m = await mr.getMovieData();
-              Moviemodel moviemodel = Moviemodel.fromJson(m);
-              print(moviemodel.plot);
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return MovieDetails(movie: moviemodel);
-              }));
-            },
-            child: Card(
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15))
-              ),
-              child: Image.network(
-                widget.searchmovie.titles[index].image,   
-                fit: BoxFit.fill
+    return BlocProvider(
+      create: (context) => MovieBloc(MovieApiRepo()),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: widget.searchmovie.titles.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: GestureDetector(
+              onTap: () async {
+                BlocProvider.of<MovieBloc>(context)
+                    .add(FetchMovie(widget.searchmovie.titles[index].id));
+                
+                //  very very important note
+                // Blocprovider.value to pass context to another screen
+                // value should be instance of existing BP --> BlocProvider.of<MovieBloc>(context)
+                // context --> context of previous screen;
+                // new screen context is underscore
+                
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return BlocProvider.value(
+                    value: BlocProvider.of<MovieBloc>(context),
+                    child: MovieDetails(),
+                  );
+                }));
+              },
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Image.network(widget.searchmovie.titles[index].image,
+                    fit: BoxFit.fill),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
-
