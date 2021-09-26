9b62aeb29dfb51fe633c34eb538770272ba157f1
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-09-26 21:53:13 +0530

    movie stack detail updated

--- blocflutter/lib/widgets/stack_movie_detail.dart
+++ blocflutter/lib/widgets/stack_movie_detail.dart
@@ -19,59 +19,60 @@ class _StackMovieState extends State<StackMovie> {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.1,
-          //margin: EdgeInsets.only(top: 110.0),
-          decoration: BoxDecoration(
-            color: Color(0xff5D3AC2),
-            borderRadius: BorderRadius.all(Radius.circular(30)),
-            // border: Border.all(color: Colors.white, width: 3),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Runtime",
-                    style: MyTheme.heading,
-                  ),
-                  Text(
-                    widget.moviemodel.length,
-                    style: MyTheme.heading,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Rating",
-                    style: MyTheme.heading,
-                  ),
-                  Text(
-                    widget.moviemodel.rating,
-                    style: MyTheme.heading,
-                  ),
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Votes",
-                    style: MyTheme.heading,
-                  ),
-                  Text(
-                    NumberFormat.compact().format(int.parse(widget.moviemodel.ratingVotes)),
-                    style: MyTheme.heading,
-                  ),
-                ],
-              ),
-            ],
-          ),
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height * 0.1,
+        //margin: EdgeInsets.only(top: 110.0),
+        decoration: BoxDecoration(
+          color: Color(0xff5D3AC2),
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+          // border: Border.all(color: Colors.white, width: 3),
         ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Runtime",
+                  style: MyTheme.heading,
+                ),
+                Text(
+                  widget.moviemodel.length,
+                  style: MyTheme.heading,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Rating",
+                  style: MyTheme.heading,
+                ),
+                Text(
+                  widget.moviemodel.rating,
+                  style: MyTheme.heading,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Votes",
+                  style: MyTheme.heading,
+                ),
+                Text(
+                  NumberFormat.compact()
+                      .format(int.parse(widget.moviemodel.ratingVotes)),
+                  style: MyTheme.heading,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
